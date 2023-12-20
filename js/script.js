25fe49f60c90c135990683fb64d1e42f75d9fc55
AlessiaOsman <alessia.osman@gmail.com>
2023-12-20 22:32:21 +0100

    added style

--- js/script.js
+++ js/script.js
@@ -6,17 +6,46 @@ const multipleFiveText = 'Buzz';
 
 console.log(multipleFiveText, multipleThreeText)
 
+const myContainer = document.getElementById('square-container')
+let square = '<div class="d-flex flex-wrap gap-3 text-center">';
+console.log(square)
+
+let squareContent = ''
+
+let bgColor = ''
+
 for (let i = 1; i <= 100; i++){
-    console.log(i)
+
+    console.log(i);
+    
+    square += `<div class='square ${bgColor} border d-flex justify-content-center align-items-center col-1'>${squareContent}</div>`
 
     if (i % 3 === 0 && i % 5 === 0){
-        console.log(multipleFiveText, multipleThreeText)
+        console.log(multipleFiveText, multipleThreeText);
+        squareContent = multipleThreeText+multipleFiveText;
+        bgColor = 'bg-danger-subtle'
+        
     } else if(i % 3 === 0){
-        console.log(multipleThreeText)
+        console.log(multipleThreeText);
+        squareContent = multipleThreeText;
+        bgColor = 'bg-success'
+
     } else if (i % 5 === 0){
-        console.log(multipleFiveText)
+        console.log(multipleFiveText);
+        squareContent = multipleFiveText;
+        bgColor = 'bg-warning'
+
     } else {
-        console.log('per nessuno')
+        squareContent = i
+        bgColor = 'bg-info'
     }
 }
+
+square += '</div>';
+
+myContainer.innerHTML = square;
+
+
+
+
  
